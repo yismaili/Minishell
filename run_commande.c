f4066039d2657106a0dc66c8694734b5b3cb4cb4
yo-ismaili <ismailiyounes20@gmail.com>
2022-08-08 22:02:04 +0100

    fix excution leaks

--- run_commande.c
+++ run_commande.c
@@ -6,7 +6,7 @@
 /*   By: yismaili < yismaili@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 11:22:25 by souchen           #+#    #+#             */
-/*   Updated: 2022/08/08 21:28:16 by yismaili         ###   ########.fr       */
+/*   Updated: 2022/08/08 21:56:25 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,10 @@ void	run_commands(t_struct *shell)
 		i++;
 	}
 	next_run_commands(shell);
-	ft_cmd(shell->commands);
 	ft_wait_pid(shell);
+	ft_cmd(shell->commands);
+	if (shell->path)
+		ft_free_cmd(shell->path);
 }
 
 void	ft_wait_pid(t_struct *shell)
